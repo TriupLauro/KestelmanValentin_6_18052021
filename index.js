49f837e082a3722d3f62cd094cda1a0a452b82e1
Valentin Kestelman <valaourok@gmail.com>
2021-05-22 14:48:26 +0200

    filtre tags de nav

--- index.js
+++ index.js
@@ -3,11 +3,30 @@
 
 let fishEyeData;
 let photographersList;
+let photographersStored = new Set();
 
 // Dom elements
 const navTags = document.querySelectorAll('div.nav-tag-item');
-//const photographersContainer = document.getElementsByClassName('photograph-wrapper')[0];
-//const photographersDisplayed = document.getElementsByClassName('photograph-profile');
+const photographersContainer = document.getElementsByClassName('photograph-wrapper')[0];
+
+// Removes the photographers elements from the homepage
+function removePhotographers () {
+    let currentElt;
+    while (photographersContainer.children.length > 0 ) {
+        currentElt = photographersContainer.removeChild(photographersContainer.children[0]);
+        photographersStored.add(currentElt);
+    }
+}
+
+function displayPhotographersList(photographersToDisplay) {
+    let namesList = photographersToDisplay.map(photographer => photographer.name);
+    console.log(namesList);
+    for (let  photographer of photographersStored) {
+        if (namesList.includes(photographer.querySelector('h2').innerText)) {
+            photographersContainer.appendChild(photographer);
+        }
+    }
+}
 
 // This function returns a promise, so we need to use .then after we call it
 function readJsonData () {
@@ -32,7 +51,7 @@ window.addEventListener('load', () => {
     .then((v) => {
         fishEyeData = v;
         photographersList = fishEyeData.photographers;
-        console.log(photographersList);
+        
     })
 });
 
@@ -45,8 +64,9 @@ navTags.forEach(tag => {
 function tagSelected(e) {
     let rawTag = e.target.textContent;
     let lowercaseTag = rawTag.toLowerCase().slice(1);
-    console.log(lowercaseTag);
+    
     let filteredPhotographers = photographersList.filter(photographer => photographer.tags.includes(lowercaseTag));
-    console.log(filteredPhotographers);
-    //photographersContainer.removeChild(photographersDisplayed[0]);
+    
+    removePhotographers();
+    displayPhotographersList(filteredPhotographers);
 }
